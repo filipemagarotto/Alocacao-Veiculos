7c306ca96a3949900516de3eb92d88f23f0107ae
Filipe Magarotto <filipemagarotto1213@gmail.com>
2022-09-12 20:26:04 -0300

    filipe push

---
+++ alocacao-carros/src/main/java/br/com/alocacao/alocacaocarros/AlocacaoCarrosApplication.java
@@ -0,0 +1,13 @@
+package br.com.alocacao.alocacaocarros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlocacaoCarrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlocacaoCarrosApplication.class, args);
+	}
+
+}
---
+++ alocacao-carros/src/test/java/br/com/alocacao/alocacaocarros/AlocacaoCarrosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alocacao.alocacaocarros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlocacaoCarrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
