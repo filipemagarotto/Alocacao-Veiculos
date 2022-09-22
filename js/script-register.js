ca12f0cb6909a5d2b2d9b839f6260564d2704bc4
Eduardo Martins <eduardo_filho2000@yahoo.com.br>
2022-09-21 23:06:03 -0300

    Ajusta PF e PJ cadastro

--- js/script-register.js
+++ js/script-register.js
@@ -1,31 +1,26 @@
-// Função para carregar os campos para cadastrar pessoa física
-function formPessoaFisica() {
-    //console.log("Carregar formulário pessoa física.");
 
-    // Apresentar o título cadastrar pessoa física
+function formPessoaFisica() {
+  
+
     document.getElementById("titulo-pessoa-fisica").style.display = 'flex';
     document.getElementById("titulo-pessoa-juridica").style.display = 'none';
 
-    // Apresentar o formulário cadastrar pessoa física
     document.getElementById("form-pessoa-fisica").style.display = 'flex';
     document.getElementById("form-pessoa-juridica").style.display = 'none';
 
-    // Carregar o botão cadastrar após o usuário selecionar o tipo de formulário pessoa física ou jurídica
+    
     document.getElementById("form-btn-cadastrar").style.display = 'flex';
 }
 
-// Função para carregar os campos para cadastrar pessoa jurídica
-function formPessoaJuridica() {
-    //console.log("Carregar formulário pessoa jurídica.");
 
-    // Apresentar o título cadastrar pessoa jurídica
+function formPessoaJuridica() {
+   
     document.getElementById("titulo-pessoa-juridica").style.display = 'flex';
     document.getElementById("titulo-pessoa-fisica").style.display = 'none';
 
-    // Apresentar o formulário cadastrar pessoa jurídica
     document.getElementById("form-pessoa-fisica").style.display = 'none';
     document.getElementById("form-pessoa-juridica").style.display = 'flex';
 
-    // Carregar o botão cadastrar após o usuário selecionar o tipo de formulário pessoa física ou jurídica
+    
     document.getElementById("form-btn-cadastrar").style.display = 'flex';
 }
