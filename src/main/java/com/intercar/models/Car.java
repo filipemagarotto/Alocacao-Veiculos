81db69a7e9f9ca796705e0bcd1ac9bef7bde2e7b
Eduardo Martins <eduardo_filho2000@yahoo.com.br>
2022-11-01 20:08:02 -0300

    Inicia model

--- src/main/java/com/intercar/models/Car.java
+++ src/main/java/com/intercar/models/Car.java
@@ -1,5 +1,59 @@
 package com.intercar.models;
 
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class Car {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+	@Column
+	private Long id;
+	
+	@Column
+	private String city_​​pick_up_location;
+	
+	@Column
+	private String specific_pick_up_location;
+	
+	@Column
+	private String car_model;
+	
+	@Column
+	private String car_color;
+	
+	@Column
+	private String withdrawn_date;
+	
+	@Column
+	private String return_date;
+	
+	@Column
+	private String year_car;
+	
+	@Column
+	private BigDecimal rental_value;
+	
+	@Column
+	private String license_plate;
+	
+	@Column
+	private String car_brand;
+	
+	@Column
+	private String km;
+	
+	@Column
+	private String gear;
+	
+	@Column
+	private Boolean air_conditioning;
+	
+	@Column
+	private String motor;
 }
