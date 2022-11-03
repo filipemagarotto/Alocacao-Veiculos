4430b7646727dacb1f181cbdc622e34cf5a8268a
Filipe Magarotto <filipemagarotto1213@gmail.com>
2022-11-02 21:42:33 -0300

    Inserindo as interfaces (Frontend)

--- src/main/java/com/intercar/controller/GenericController.java
+++ src/main/java/com/intercar/controller/GenericController.java
@@ -1,8 +1,50 @@
 package com.intercar.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@Controller
 public class GenericController {
 
+	@RequestMapping("/")
+	public String raiz() {
+		return "home";
+	}
+	
+	@GetMapping("/home")
+	public String home(Model model) {
+		return "home";
+	}
+	
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+	
+	@GetMapping("/minhasReservas")
+	public String minhasReservas() {
+		return "minhasReservas";
+	}
+	
+	@GetMapping("/listCarsByFilter")
+	public String listCarsByFilter() {
+		return "listCarsByFilter";
+	}
+	
+	@GetMapping("/carPreview")
+	public String carPreview() {
+		return "carPreview";
+	}
+	
+	@GetMapping("/anuncie")
+	public String anuncie() {
+		return "anuncie";
+	}
+	
+	@GetMapping("/profile")
+	public String profile() {
+		return "profile";
+	}
 }
--- src/main/java/com/intercar/project/IntercarApplication.java
+++ src/main/java/com/intercar/project/IntercarApplication.java
@@ -3,7 +3,7 @@ package com.intercar.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.intercar.controller")
 public class IntercarApplication {
 
 	public static void main(String[] args) {
